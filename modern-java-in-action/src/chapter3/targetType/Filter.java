015229b2e5694c840606d75667ecce4f56cab324
2jaeheon <jaeheon0826@jbnu.ac.kr>
2025-10-05 18:07:03 +0900

    add: lambda basic
    Signed-off-by: 2jaeheon <jaeheon0826@jbnu.ac.kr>

---
+++ modern-java-in-action/src/chapter3/targetType/Filter.java
@@ -0,0 +1,13 @@
+package chapter3.targetType;
+
+public class Filter {
+
+    //List<Apple> heavierThan150g = filter(inventory, (Apple apple) -> apple.getWeight() > 150);
+
+    // 1.filter 메서드 선언을 확인
+    // 2. filter 메서드는 두 번째 파라미터로 Predicate<Apple> 형식을 기대
+    // 3. Predicate<Apple> 은 test라는 한 개의 추상 메서드를 정의하는 함수형 인터페이스
+    // 4. test 메서드는 Apple을 받아서 boolean을 반환하는 함수 디스크립터를 묘사
+    // 5. filter 메서드로 전달된 인수는 요구사항을 만족해야함.
+
+}
